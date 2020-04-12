650b99b22325597591cba64e870c3c4c4404d55c
= <=>
2020-04-12 11:03:47 -0500

    finished step 4

--- src/Routes.js
+++ src/Routes.js
@@ -3,11 +3,14 @@ import { Switch, Route } from "react-router-dom";
 
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
+import ClassList from "./components/ClassList/ClassList";
+
 export default function Routes() {
   return (
     <Switch>
       <Route exact path="/" component={Home} />
       <Route path="/about" component={About} />
+      <Route path="/classlist/:class" component={ClassList} />
     </Switch>
   );
 }
