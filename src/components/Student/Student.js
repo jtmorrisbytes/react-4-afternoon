224622e1c7331082e00da2072875a9759c92c447
= <=>
2020-04-12 13:40:55 -0500

    finished step 7

--- src/components/Student/Student.js
+++ src/components/Student/Student.js
@@ -1,14 +1,41 @@
 import React, { Component } from "react";
+import Axios from "axios";
 
 export default class Student extends Component {
   constructor() {
     super();
+    this.state = {
+      studentInfo: {},
+    };
   }
-
+  componentDidMount() {
+    let id = ((this.props.match || {}).params || {}).id;
+    if (id) {
+      Axios.get(`/students/${id}`)
+        .then((response) => {
+          this.setState({ studentInfo: response.data });
+        })
+        .catch((error) => {
+          if (error.response.status === 404) {
+            alert(`The Student with id '${id}' was not found`);
+          }
+          console.error(error);
+          try {
+            this.props.history.push("/");
+          } catch (e) {}
+        });
+    }
+  }
   render() {
+    let { first_name, last_name, grade, email } = this.state.studentInfo || {};
     return (
       <div className="box">
         <h1>Student</h1>
+        <h1>
+          {first_name} {last_name}
+        </h1>
+        <h3>Grade: {grade}</h3>
+        <h3>Email: {email}</h3>
       </div>
     );
   }
